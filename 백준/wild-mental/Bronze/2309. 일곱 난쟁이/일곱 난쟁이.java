bc0114d39999d34f242f2008e03bdd76457af2e5
wild-mental <wild.mental@kidsnote.com>
2024-01-28 23:59:22 +0900

    사용 디렉토리 변경

---
+++ 백준/wild-mental/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+	
+	static int sum = 0;
+	
+	public static void solution(int[] input) {
+		
+		for(int i=0;i<8;i++) {
+			for(int j=i+1;j<9;j++) {
+				if((sum-input[i]-input[j] == 100)) {
+					input[i] = input[j] = 100;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] input = new int[9];
+		for(int i=0;i<9;i++) {
+			input[i] = Integer.parseInt(br.readLine());
+			sum += input[i];
+		}
+		solution(input);
+		Arrays.sort(input);
+		for(int i=0;i<7;i++) {
+			System.out.println(input[i]);
+		}
+	}
+}
